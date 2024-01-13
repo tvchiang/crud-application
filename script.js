53344444475a7eaf545fc8c1781e027882330f5a
sh3ngsh3ng <yshenglim13@gmail.com>
2024-01-13 04:20:30 +0000

    added CRUD in data.js

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // all the code here
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Sanity check
 // console.log("hello is linked")
 // alert("Hello!")
+
+// DOM = Document Object Model
+
+// DOM Manipulation
+// const btn = document.querySelector("#dom-btn") // # => id
+// btn.addEventListener("click", function() {
+//     alert("button is clicked")
+// })
